262356808f49f0db7edf2d6f0b24219ccfdd5dae
Edson Ivo <edson.ivo@alu.ufc.br>
2021-06-25 18:58:09 -0300

    Enviando layout

---
+++ ProgramasMobile/app/src/main/java/com/example/cadernodoprodutor/DadosPropriedade.kt
@@ -0,0 +1,11 @@
+package com.example.cadernodoprodutor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DadosPropriedade : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dados_propriedade)
+    }
+}
